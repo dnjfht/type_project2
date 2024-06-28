a5c11b1827061d58cd63d616a7d1ecfa2f6ec1aa
Yoo Seungmin <dnjfht@gmail.com>
2024-06-28 18:40:20 +0900

    React · Typescript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import * as D from './data'
-
 function App() {
-  return (
-    <div>
-      <p>
-        {D.randomName()}, {D.randomJobTitle()}, {D.randomDatMonthYear()}
-      </p>
-      <img src={D.randomAvatar()} height="50" />
-      <img src={D.randomImage()} height="300" />
-    </div>
-  )
+  return <></>
 }
 
 export default App
