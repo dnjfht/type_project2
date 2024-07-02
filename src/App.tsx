65767316a9d1180add8488773ca9bee902066651
Yoo Seungmin <dnjfht@gmail.com>
2024-07-02 19:02:24 +0900

    Arrays and JSX syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 function App() {
-  return <div>Hello world!</div>
+  const arr = [
+    <li>
+      <a href="http://www.google.com" target="_blank">
+        <p>go to Google</p>
+      </a>
+    </li>,
+    <li>
+      <a href="http://www.facebook.com" target="_blank">
+        <p>go to Facebook</p>
+      </a>
+    </li>,
+    <li>
+      <a href="http://www.twitter.com" target="_blank">
+        <p>go to Twitter</p>
+      </a>
+    </li>
+  ]
+
+  return (
+    <div>
+      <ul>{arr}</ul>
+    </div>
+  )
 }
 
 export default App
