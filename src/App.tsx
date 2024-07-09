1841630d452a8c07627815be6db0836388471972
Yoo Seungmin <dnjfht@gmail.com>
2024-07-10 02:04:32 +0900

    React study : Making a data array into a component array.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
 function App() {
-  const arr = [
-    <li>
-      <a href="http://www.google.com" target="_blank">
-        <p>go to Google</p>
-      </a>
-    </li>,
-    <li>
-      <a href="http://www.facebook.com" target="_blank">
-        <p>go to Facebook</p>
-      </a>
-    </li>,
-    <li>
-      <a href="http://www.twitter.com" target="_blank">
-        <p>go to Twitter</p>
-      </a>
-    </li>
-  ]
-
-  return (
-    <div>
-      <ul>{arr}</ul>
-    </div>
-  )
+  return <div>Hello world!</div>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import * as D from './data'
+
+const children = D.makeArray(10).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+    <p>{D.randomSentence()}</p>
+    <img src={D.randomAvatar()} width={100} height={100} />
+  </div>
+))
+
+const rootVirtualDom = <div>{children}</div>
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+root.render(rootVirtualDom)
