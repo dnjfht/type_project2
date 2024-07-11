9c4229ad337d2196539047610b195a80d2f6ed2b
Yoo Seungmin <dnjfht@gmail.com>
2024-07-12 04:59:42 +0900

    study : Get properties a little more concisely using decomposition assignments.

--- src/ClassComponent.tsx
+++ src/ClassComponent.tsx
@@ -7,8 +7,11 @@ type ClassComponentProps = {
 
 export default class ClassComponent extends Component<ClassComponentProps> {
   render() {
-    const href = this.props.href
-    const text = this.props.text
+    // const href = this.props.href
+    // const text = this.props.text
+
+    // 비구조화 할당 구문(구조분해 할당)을 통하여 좀 더 간략하게 코드 작성.
+    const {href, text} = this.props
 
     return (
       <li>
