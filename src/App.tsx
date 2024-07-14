454e3e4d0609a89c417091cff538fb44efbef530
Yoo Seungmin <dnjfht@gmail.com>
2024-07-14 15:15:33 +0900

    study : Using Event Properties in the Physical DOM.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
 import P from './P'
+import DispatchEvent from './pages/DispatchEvent'
+import DragDrop from './pages/DragDrop'
+import EventBubbling from './pages/EventBubbling'
+import EventListener from './pages/EventListener'
+import FileDrop from './pages/FileDrop'
+import FileInput from './pages/FileInput'
+import OnChange from './pages/OnChange'
+import OnClick from './pages/OnClick'
+import ReactOnClick from './pages/ReactOnClick'
+import StopPropagation from './pages/StopPropagation'
+import VariousInputs from './pages/VariousInputs'
 
-export default function App() {
-  const texts = ['hello', 'world', 'ok?', "I'm very happy"].map((text, index) => (
-    <P key={index} children={text} />
-  ))
-  return <div children={texts} />
+const App = (): JSX.Element => {
+  // window.addEventListener('click', (e: Event) => console.log('mouse click occurs.'))
+
+  // document.getElementById('root')?.addEventListener('click', (e: Event) => {
+  //   const {isTrusted, target, bubbles} = e
+  //   console.log('mouse click occurs.', isTrusted, target, bubbles)
+  // })
+
+  return (
+    <div>
+      <FileDrop />
+      <DragDrop />
+      <FileInput />
+      <OnChange />
+      <VariousInputs />
+      <StopPropagation />
+      <EventBubbling />
+      <DispatchEvent />
+      <ReactOnClick />
+      <OnClick />
+      <EventListener />
+    </div>
+  )
 }
+
+export default App
