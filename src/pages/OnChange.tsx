fdf41f208328749b4ab44952ac9ca8d89372a960
Yoo Seungmin <dnjfht@gmail.com>
2024-07-18 16:29:58 +0900

    study : Applying the onChange event to different types of inputs. text, email, password, range, checkbox, radio, file ...

--- src/pages/OnChange.tsx
+++ src/pages/OnChange.tsx
@@ -1,3 +1,42 @@
+import {ChangeEvent} from 'react'
+
 export default function OnChange() {
-  return <div>OnChange</div>
+  const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+    e.preventDefault()
+
+    console.log('onChangeValue', e.target.value)
+  }
+
+  const onChangeChecked = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+
+    console.log('onChangeValue', e.target.checked)
+  }
+
+  const onChangeFiles = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+
+    console.log('onChangeValue', e.target.files)
+  }
+
+  return (
+    <div>
+      <p>OnChange</p>
+
+      <input
+        type="text"
+        onChange={onChangeValue}
+        placeholder="type some text"
+        defaultValue="hello"
+      />
+      <input
+        type="checkbox"
+        onChange={onChangeChecked}
+        placeholder="type some text"
+        defaultChecked
+      />
+      <input type="file" onChange={onChangeFiles} multiple accept="images/*" />
+    </div>
+  )
 }
--- src/pages/VariousInputs.tsx
+++ src/pages/VariousInputs.tsx
@@ -1,3 +1,33 @@
+import {ChangeEvent, useState} from 'react'
+
 export default function VariousInputs() {
-  return <div>VariousInputs</div>
+  const [text, setText] = useState<string>('hello')
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <div>
+      <p>VariousInputs</p>
+      <p>{text}</p>
+
+      <div>
+        <input
+          type="text"
+          placeholder="enter some texts"
+          onChange={onChangeText}
+          value={text}
+          // defaultValue="hello"
+        />
+        <input type="password" placeholder="enter your password" />
+        <input type="email" placeholder="enter email address" />
+        <input type="range" />
+        <input type="button" value="I'm a button" />
+        <input type="checkbox" value="I'm a checkbox" defaultChecked />
+        <input type="radio" value="I'm a radio" defaultChecked />
+        <input type="file" />
+      </div>
+    </div>
+  )
 }
