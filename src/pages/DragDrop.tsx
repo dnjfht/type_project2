b5444ea38fbe50089c04e684492b9f4fd275a973
Yoo Seungmin <dnjfht@gmail.com>
2024-07-19 04:15:46 +0900

    study : Applying image and file drag-and-drop events.

--- src/pages/DragDrop.tsx
+++ src/pages/DragDrop.tsx
@@ -1,3 +1,35 @@
+import {DragEvent} from 'react'
+
 export default function DragDrop() {
-  return <div>DragDrop</div>
+  const onDragStart = (e: DragEvent<HTMLElement>) => {
+    console.log('OnDragStart', e.dataTransfer)
+  }
+
+  const onDragEnd = (e: DragEvent<HTMLElement>) => {
+    console.log('OnDragEnd', e.dataTransfer)
+  }
+
+  const onDragOver = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault()
+    console.log('OnDragOver', e.dataTransfer)
+  }
+
+  const onDrop = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault()
+    console.log('OnDrop', e.dataTransfer)
+  }
+
+  return (
+    <div>
+      <p>DragDrop</p>
+
+      <div draggable onDragStart={onDragStart} onDragEnter={onDragEnd}>
+        <h1>Drag Me</h1>
+      </div>
+
+      <div draggable onDrop={onDrop} onDragOver={onDragOver}>
+        <h1>Drag over Me</h1>
+      </div>
+    </div>
+  )
 }
--- src/pages/FileDrop.tsx
+++ src/pages/FileDrop.tsx
@@ -1,3 +1,31 @@
+import {DragEvent} from 'react'
+
 export default function FileDrop() {
-  return <div>FileDrop</div>
+  const onDragOver = (e: DragEvent) => {
+    e.preventDefault()
+
+    console.log('dragOVer', e.dataTransfer)
+  }
+
+  const onDrop = (e: DragEvent) => {
+    e.preventDefault()
+
+    const files = e.dataTransfer?.files
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file: File | null = files.item(i) // or file = files[i]
+        console.log(`file[${i}] drop!`, file)
+      }
+    }
+  }
+
+  return (
+    <div>
+      <p>FileDrop</p>
+
+      <div onDrop={onDrop} onDragOver={onDragOver}>
+        <h1>Drop image files over Me!</h1>
+      </div>
+    </div>
+  )
 }
